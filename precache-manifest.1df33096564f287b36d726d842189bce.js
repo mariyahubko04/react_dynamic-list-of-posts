71c6853ed2222eda9f52d2e66c751944c316cd3a
unknown <mariia>
2019-07-18 15:21:51 +0300

    Updates

---
+++ precache-manifest.1df33096564f287b36d726d842189bce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15cec2f5160dcc3b4562b53fcdf8c913",
+    "url": "/react_dynamic-list-of-posts/index.html"
+  },
+  {
+    "revision": "a36b4bf9f330dbd6a888",
+    "url": "/react_dynamic-list-of-posts/static/css/main.614aed70.chunk.css"
+  },
+  {
+    "revision": "5f5a53db3ca51547e5fd",
+    "url": "/react_dynamic-list-of-posts/static/js/2.0122b471.chunk.js"
+  },
+  {
+    "revision": "a36b4bf9f330dbd6a888",
+    "url": "/react_dynamic-list-of-posts/static/js/main.929d84b5.chunk.js"
+  },
+  {
+    "revision": "fc988510490290486853",
+    "url": "/react_dynamic-list-of-posts/static/js/runtime~main.69709ad2.js"
+  }
+]);
